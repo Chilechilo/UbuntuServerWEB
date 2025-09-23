d087bc7b123644c087386ac4c0f64692bef76068
Luis <luisangelhc2004@hotmail.com>
2025-09-22 19:07:47 -0600

    sad

--- django/webLuis/mcm_archives/edwinapp/urls.py
+++ django/webLuis/mcm_archives/edwinapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("saludo/<str:name>/", views.saludo, name="saludo"),
+    path("<str:name>", views.saludo, name="saludo"),
 ]
