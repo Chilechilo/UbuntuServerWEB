9f6d88d08f5416bd7b2a5060fd4dd3f98b3be535
Luis <luisangelhc2004@hotmail.com>
2025-09-22 18:46:35 -0600

    Se agrega index y saludo.html

--- django/webLuis/mcm_archives/edwinapp/urls.py
+++ django/webLuis/mcm_archives/edwinapp/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('suma/', views.suma, name='suma'),
-    path('suma/<int:a>/<int:b>/', views.suma_path, name='suma_path'),
-    path('pagina/', views.pagina, name='pagina')
+    path("<str:name>", views.saludo, name="saludo")
 ]
