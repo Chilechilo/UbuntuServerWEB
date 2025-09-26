e4f5565047c99687214950646d15857e575eff06
Luis <luisangelhc2004@hotmail.com>
2025-09-25 19:47:58 -0600

    Se soluciona error

--- django/webLuis/mcm_archives/edwinapp/models.py
+++ django/webLuis/mcm_archives/edwinapp/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Task (models.Model):
-    title = models.CharField(max_lenght=200)
+    title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
