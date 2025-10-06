6483e413a58dfaf22ee8ef06c233ecd6e1c89fa4
Luis <luisangelhc2004@hotmail.com>
2025-10-06 17:57:31 -0600

    Se agrega clase usuario

--- django/webLuis/mcm_archives/edwinapp/models.py
+++ django/webLuis/mcm_archives/edwinapp/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class Task (models.Model):
     title = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    edad = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.nombre} ({self.edad})"
